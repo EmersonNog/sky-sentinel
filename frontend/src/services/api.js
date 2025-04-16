828db9362a17198e402c7072ae16b5d0a46e33b9
Emerson <emersonnogueira54321@gmail.com>
2025-04-16 14:02:02 -0300

    fix: Set new base_url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,14 @@
 // src/services/api.js
 import axios from "axios";
 
+const BASE_URL = "https://sky-sentinel-production.up.railway.app/";
+
 export const fetchSatellites = async (ids) => {
   if (!ids || ids.length === 0) return [];
 
   try {
     const res = await axios.get(
-      `http://localhost:3001/api/satellites?ids=${ids.join(",")}`
+      `${BASE_URL}/api/satellites?ids=${ids.join(",")}`
     );
     return res.data;
   } catch (error) {
@@ -17,7 +19,7 @@ export const fetchSatellites = async (ids) => {
 
 export const fetchSatellitesList = async () => {
   try {
-    const res = await axios.get("http://localhost:3001/api/satellites-list");
+    const res = await axios.get(`${BASE_URL}/api/satellites-list`);
     return res.data;
   } catch (error) {
     console.error("Erro ao buscar lista de satélites:", error);
